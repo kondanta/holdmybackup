6faeaada4d058c55e61afdb9b7e14171c1ba25cb
Taylan Dogan <git@taylandogan.info>
2021-09-08 23:56:05 +0300

    refactor(config): disable verbosity from config file
    Signed-off-by: Taylan Dogan <git@taylandogan.info>

--- src/config/config_file.rs
+++ src/config/config_file.rs
@@ -1,20 +1,10 @@
 use {
     anyhow::Result,
-    crossbeam_channel,
-    notify::{
-        event::{
-            AccessKind,
-            AccessMode,
-        },
-        RecommendedWatcher,
-        RecursiveMode,
-        Watcher,
-    },
+    reload_config,
     serde::Deserialize,
     std::{
         fs::File,
         io::BufReader,
-        path::Path,
         sync::{
             Arc,
             Mutex,
@@ -60,60 +50,30 @@ pub struct Storage {
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct Config {
-    pub backup:    Backup,
-    pub storage:   Storage,
-    pub verbosity: String,
+    pub backup:  Backup,
+    pub storage: Storage,
 }
 
 impl Config {
     pub fn load_config() -> Result<Config, anyhow::Error> {
-        let f = BufReader::new(File::open("./config.yaml")?);
+        let opt = crate::config::args::Opt::args();
+        let f = BufReader::new(File::open(opt.config_path)?);
         serde_yaml::from_reader(f).map_err(|e| {
             anyhow::anyhow!("Cannot parse the config file: {}", e.to_string())
         })
     }
 
-    pub fn watch_config_changes(cfg: Arc<Mutex<Config>>) -> Result<()> {
-        std::thread::spawn(move || loop {
-            let (tx, rx) = crossbeam_channel::unbounded();
-
-            let mut watcher: RecommendedWatcher = RecommendedWatcher::new(tx)
-                .map_err(|e| anyhow::anyhow!("Cannot create watcher: {:#?}", e))
-                .expect("Cannot create Watcher");
-
-            watcher
-                .watch(Path::new("config.yaml"), RecursiveMode::NonRecursive)
-                .expect("Cannot listen configuration file. Exiting...");
-
-            match rx.recv().map_err(|e| {
-                anyhow::anyhow!("Receiving error on config watcher: {:#?}", e)
-            }) {
-                Ok(event) => match event {
-                    Ok(e) => {
-                        if e.kind ==
-                            notify::EventKind::Access(AccessKind::Close(
-                                AccessMode::Write,
-                            ))
-                        {
-                            match Config::load_config() {
-                                Ok(new_config) => {
-                                    *cfg.lock().unwrap() = new_config
-                                }
-                                Err(e) => {
-                                    tracing::error!(
-                                        "Error reloading config: {:#?}",
-                                        e
-                                    )
-                                }
-                            }
-                        }
-                    }
-                    Err(e) => tracing::error!("Event error: {:#?}", e),
-                },
-                Err(e) => tracing::error!("Watch error: {:#?}", e.to_string()),
-            }
-        });
-        Ok(())
+    pub fn watch_config_changes(
+        cfg: Arc<Mutex<Config>>,
+        mode: String,
+        config_path: String,
+    ) -> Result<()> {
+        reload_config::watch_changes(
+            cfg,
+            mode,
+            config_path,
+            Config::load_config,
+        )
     }
 }
 

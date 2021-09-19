331b1047c895e2f45ae2a328d87c4dc728e597c6
Taylan Dogan <git@taylandogan.info>
2021-09-19 22:24:02 +0300

    refactor(args): add possible values
    Signed-off-by: Taylan Dogan <git@taylandogan.info>

--- src/config/args.rs
+++ src/config/args.rs
@@ -1,13 +1,22 @@
+use lazy_static::lazy_static;
 use structopt::StructOpt;
 
+lazy_static! {
+    static ref PV: Vec<&'static str> = vec!["recursive", "nonrecursive"];
+}
+
 #[derive(StructOpt, Debug)]
-#[structopt(name = "Hold My Backup")]
+#[structopt(
+    name = "Hold My Backup",
+    about = "A cloud native backup application which creates and stores \
+             backups."
+)]
 pub struct Opt {
     /// HTTP Server address
     #[structopt(long, default_value = "127.0.0.1:9090")]
     pub address: String,
 
-    /// Sets which path application should look for config file. Config file
+    /// Sets which path application should look for a config file. Config file
     /// extension must be provided as well.
     #[structopt(short, long, default_value = "./config.yaml")]
     pub config_path: String,
@@ -15,7 +24,11 @@ pub struct Opt {
     /// Sets whether config reload logic listens folder recursively, like
     /// editing sub-folder also triggers an event, or listen just the given
     /// path.
-    #[structopt(long, default_value = "nonrecursive")]
+    #[structopt(
+        long,
+        default_value = "nonrecursive",
+        possible_values(&PV),
+    )]
     pub recursive_mode: String,
 
     /// Initial log level of the application
